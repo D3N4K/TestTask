cd43c82e8bbae60d482ce8ad2096fb3aa619dce5
D3N4K <gerasichev2005@gmail.com>
2025-04-19 18:42:18 +0300

    Добавьте файлы проекта.

---
+++ TestTskApp/Classes/InfoObject.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TestTskApp.Classes
+{   
+    internal class InfoObject
+    {
+        public string Name { get; set; }
+        public float Distance { get; set; }
+        public float Angle { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public string IsDefect { get; set; }
+    public InfoObject(string name, float distance, float angle, float height, float width, string isDefect)
+        {
+            Name = name;
+            Distance = distance;
+            Angle = angle;
+            Height = height;
+            Width = width;
+            IsDefect = isDefect;
+        }
+        public InfoObject()
+        {
+        }
+        public List<InfoObject> ImportExcel(string filePath)
+        {
+            List<InfoObject> infoObjects = new List<InfoObject>();
+            var app = new Excel.Application();
+            try
+            {
+                Excel.Workbook workbook = app.Workbooks.Open(filePath);
+                Excel.Worksheet worksheet = workbook.Sheets[1];
+                int maxRow = worksheet.UsedRange.Rows.Count;
+                for(int row = 2; row <= maxRow; row++)
+                {
+                    infoObjects.Add(new InfoObject(worksheet.Cells[row, 1].Text, float.Parse(worksheet.Cells[row, 2].Text), float.Parse(worksheet.Cells[row, 3].Text), float.Parse(worksheet.Cells[row, 4].Text), float.Parse(worksheet.Cells[row, 5].Text), worksheet.Cells[row, 6].Text));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                app.Quit();
+            }
+            return infoObjects;
+        }
+        public List<InfoObject> ImportCSV(string filePath)
+        {
+            List<InfoObject> infoObjects = new List<InfoObject>();
+            try
+            {
+                StreamReader reader = new StreamReader(filePath);
+                var headers = reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    infoObjects.Add(new InfoObject(values[0].ToString(), float.Parse(values[1].ToString()), float.Parse(values[2].ToString()), float.Parse(values[3].ToString()), float.Parse(values[4].ToString()), values[5].ToString()));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return infoObjects;
+        }
+    }
+}
---
+++ TestTskApp/Windows/DataWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TestTskApp.Classes;
+
+namespace TestTskApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+        string filePath;
+        List<InfoObject> infoObjects = new List<InfoObject>();
+        List<InfoObject> infoObjectsCsv = new List<InfoObject>();
+        List<InfoObject> infoObjectsExcel = new List<InfoObject>();
+        InfoObject infoObject = new InfoObject();
+        int selectedRow = -1;
+        InfoObject currentObject = new InfoObject();
+        public DataWindow()
+        {
+            InitializeComponent();
+        }
+        private void miImportCsv_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+            infoObjectsCsv = infoObject.ImportCSV(filePath);
+            infoObjects.AddRange(infoObjectsCsv);
+            ViewData();
+        }
+        private void miImportExcel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+            infoObjectsExcel = infoObject.ImportExcel(filePath);
+            infoObjects.AddRange(infoObjectsExcel);
+            ViewData();
+        }
+        void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            filePath = openFileDialog.FileName;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        void ViewData()
+        {
+            dgData.DataContext = infoObjects;
+            dgData.SetBinding(System.Windows.Controls.DataGrid.ItemsSourceProperty, new System.Windows.Data.Binding() { Path = new PropertyPath(".") });
+        }
+
+        private void dgData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedRow = dgData.SelectedIndex;
+            if (selectedRow > -1)
+            {
+                currentObject = infoObjects[selectedRow];
+                tbName.Text = $"Name: {currentObject.Name}";
+                tbDistance.Text = $"Distance: {currentObject.Distance}";
+                tbAngle.Text = $"Angle: {currentObject.Angle}";
+                tbWidth.Text = $"Width: {currentObject.Width}";
+                tbHeight.Text = $"Height: {currentObject.Height}";
+                tbIsDefect.Text = $"IsDefect: {currentObject.IsDefect}";
+                canvasObject.Children.Clear();
+                DrawCanvas(currentObject.Distance, currentObject.Angle, currentObject.Height, currentObject.Width);
+            }
+        }
+        void DrawCanvas(float distance, float angle, float height, float width)
+        {
+            double x = distance * (canvasObject.ActualWidth / 20);
+            double y = (12 - angle) * (canvasObject.ActualHeight / 12);
+            double objWidth = width * (canvasObject.ActualWidth / 20);
+            double objHeight = height * (canvasObject.ActualHeight / 12);
+            Rectangle rectangle = new Rectangle
+            {
+                Width = objWidth,
+                Height = objHeight,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.LightBlue
+            };
+            Canvas.SetLeft(rectangle, x);
+            Canvas.SetTop(rectangle, y);
+            canvasObject.Children.Add(rectangle);
+        }
+    }
+}
